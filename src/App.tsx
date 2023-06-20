f4e76728ae9b4a3c638efb6c0a804b4c6e2d1a09
mvl3x <125395126+mvl3x@users.noreply.github.com>
2023-06-20 18:26:09 +0500

    feat: implement basic functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+type TCounter = {
+  currentValue: number;
+  minValue: number;
+  maxValue: number;
+};
 
 function App() {
+  const [counter, setCounter] = useState<TCounter>({
+    currentValue: 0,
+    minValue: 0,
+    maxValue: 10,
+  });
+
   return (
     <div className="App">
-
+      <div>{counter.currentValue}</div>
+      <button
+        onClick={() =>
+          setCounter({ ...counter, currentValue: counter.currentValue + 1 })
+        }
+        disabled={counter.currentValue === counter.maxValue}
+      >
+        inc
+      </button>
+      <button
+        onClick={() =>
+          setCounter({ ...counter, currentValue: counter.minValue })
+        }
+        disabled={counter.currentValue === counter.minValue}
+      >
+        reset
+      </button>
     </div>
   );
 }
