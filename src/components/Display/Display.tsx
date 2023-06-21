f999fc3d7d7e5ee4775c594156f0f2de1302130c
mvl3x <125395126+mvl3x@users.noreply.github.com>
2023-06-21 14:58:55 +0500

    feat: add styles for Display

--- src/components/Display/Display.tsx
+++ src/components/Display/Display.tsx
@@ -1,9 +1,17 @@
 import { FC, ReactNode } from "react";
+import { TCounter } from "../../App";
+import s from "./Display.module.css";
 
 type TDisplayProps = {
+  counter: TCounter;
   children: ReactNode;
 };
 
-export const Display: FC<TDisplayProps> = ({ children }) => {
-  return <div>{children}</div>;
+export const Display: FC<TDisplayProps> = ({ counter, children }) => {
+  const displayClass =
+    counter.currentValue === counter.maxValue
+      ? `${s.display} ${s.max}`
+      : s.display;
+
+  return <div className={displayClass}>{children}</div>;
 };
